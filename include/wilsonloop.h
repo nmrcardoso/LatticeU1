ade22d9ca0c17ae900e541c9c757afc76eee36b7
Nuno Cardoso <nmrcardoso@gmail.com>
2021-03-19 14:34:50 +0000

    Update wilsonloop.h
    Added space-space wilson loop

--- include/wilsonloop.h
+++ include/wilsonloop.h
@@ -13,6 +13,10 @@ Array<complexd>* WilsonLoop(Array<Real>* lat, int R, int T, bool FastVersion=tru
 template<class Real>
 void WilsonLoop(Array<Real>* lat, Array<complexd>** wl, Array<complexd>** wlfield, int R, int T, bool FastVersion);
 
+  
+template<class Real>
+Array<complexd>* WilsonLoopSS(Array<Real>* lat, int R, int T);
+
 
 }
 
